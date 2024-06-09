68d3462c54b9a37e3a4cba70eb9d2c753564527f
Farid <mdfaridhossenrehad@gmail.com>
2024-06-10 00:42:02 +0600

    4

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,68 @@
 
+import { useLocation, useNavigate } from "react-router";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
+import axios from "axios";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useCart from "../../Hooks/useCart";
+
 
 const FoodCard = ({item}) => {
+                    
     const { name, image, price, recipe, _id } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
+
+    const handleAddToCard = () =>{
+        if(user && user.email){
+            // send cart item to the database
+            const cartItem = {
+                menuId : _id,
+                email: user.email,
+                name,
+                image,
+                price
+            }
+            axiosSecure.post('/carts', cartItem)
+                .then(res =>{
+                    console.log(res.data)
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `${name} added to your cart`,
+                            showConfirmButton: false,
+                            timer: 2300
+                        });
+                        //refetch cart to update the cart items count
+                        refetch();
+                        
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: "You are not Logged In",
+                text: "Please login to add to the cart?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    //   send the user to the login page
+                    navigate('/login', {state: {from: location}})
+                }
+            });
+        }
+
+    }
+
+    
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -11,6 +72,7 @@ const FoodCard = ({item}) => {
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
                     <button
+                        onClick={handleAddToCard}
                         className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4"
                     >Add to Cart</button>
                 </div>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAc5Hl2JRPbjtR2B2u4XC8sMYuG5x_xhZI",
+  authDomain: "bistro-boss-2e70f.firebaseapp.com",
+  projectId: "bistro-boss-2e70f",
+  storageBucket: "bistro-boss-2e70f.appspot.com",
+  messagingSenderId: "953131828977",
+  appId: "1:953131828977:web:46d10cbeeb12ebb08bcb50"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
